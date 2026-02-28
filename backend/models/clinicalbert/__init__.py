2080749d4ba60ebfaeba814bb54a9d81ed0dc8b3
kinghusny <husnynajeeb@gmail.com>
2026-03-01 02:48:23 +0530

    Adding placeholder for model from main
    Adding placeholder for model from main

---
+++ backend/models/clinicalbert/__init__.py
@@ -0,0 +1,8 @@
+
+"""
+This folder is meant to contain the finetuned ClinicalBERT model files 
+for extraction and interpretation tasks. 
+
+These files are not included in the repository due to size. 
+Team members should download the model files and place them here.
+"""
